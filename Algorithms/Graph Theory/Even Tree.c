f03d5ed52f694c7d64277cad8d692feea6fac240
Paul Bartlett <bartlett_paul@hotmail.com>
2016-06-29 16:18:35 -0400

    initial graph theory

---
+++ Algorithms/Graph Theory/Even Tree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+typedef struct node {
+    struct node *left;
+    struct node *right;
+    char *string;
+} node;
+
+int main() {
+
+        
+    return 0;
+}
