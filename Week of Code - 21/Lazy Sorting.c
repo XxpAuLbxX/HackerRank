9d2bd218448eca543b85bc7bc133a5020ae6636a
Paul Bartlett <bartlett_paul@hotmail.com>
2016-06-28 20:27:36 -0400

    improvements, only works for some cases

--- Week of Code - 21/Lazy Sorting.c
+++ Week of Code - 21/Lazy Sorting.c
@@ -5,15 +5,24 @@
 
 int main() {
     char buf[81], *ptr;
-    int n, i = 0, P[18];
+    int j = 0, P[18]; 
+    double n, sort, sum = 0.0, i = 1.0;
     // read in n
     fgets(buf, sizeof(buf), stdin); 
     buf[strcspn(buf, "\n")] = 0;
     n = strtol(buf, &ptr, 10);
     // read in Pi
-    while(i < n) {
-        scanf("%d", &P[i]);
+    while(j < n) {
+        scanf("%d", &P[j]);
+        j++;
+    }
+    // While loop to run until precision is lost
+    sort = 1.0 / n;
+    while(sort > 0.0000000001) { 
+        sum += (sort * i);
+        sort /= n; // next probability
         i++;
     }
+    printf("%0.6f", sum);
     return 0;
 }
