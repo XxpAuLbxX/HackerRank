5d1cbf98a432f9a99acfc8af6e8b11fbcf29382b
Paul Bartlett <bartlett_paul@hotmail.com>
2016-07-22 13:42:11 -0400

    Initial code

---
+++ Mathematics/Fundamentals/Reverse Game.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int T = scan.nextInt(), N, K;
+        for(int i = 0; i < T; i++) {
+            N = scan.nextInt();
+            K = scan.nextInt();
+    }
+}
