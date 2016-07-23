5b5e8d9ee61fd96e6b140ba7c97f602dd4747a9a
Paul Bartlett <bartlett_paul@hotmail.com>
2016-07-22 21:53:03 -0400

    More updates

--- Mathematics/Fundamentals/Reverse Game.java
+++ Mathematics/Fundamentals/Reverse Game.java
@@ -6,8 +6,19 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int T = scan.nextInt(), N, K;
+        int[] reverse;
         for(int i = 0; i < T; i++) {
             N = scan.nextInt();
             K = scan.nextInt();
+            reverse = new int[N];
+            for(int j = 0; j < N; j++) {
+                if(j%2 == 0) {
+                    reverse[j] = N-1-(j/2);
+                } else {
+                    reverse[j] = (j/2);
+                }
+            }
+            System.out.println(reverse[K]);
+        }
     }
 }
