a0c574180ef9d27f161c620da743110e92a7fda3
Paul Bartlett <bartlett_paul@hotmail.com>
2016-07-23 20:33:39 -0400

    Completed and commented

--- Mathematics/Fundamentals/Reverse Game.java
+++ Mathematics/Fundamentals/Reverse Game.java
@@ -6,19 +6,20 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int T = scan.nextInt(), N, K;
-        int[] reverse;
         for(int i = 0; i < T; i++) {
             N = scan.nextInt();
             K = scan.nextInt();
-            reverse = new int[N];
-            for(int j = 0; j < N; j++) {
-                if(j%2 == 0) {
-                    reverse[j] = N-1-(j/2);
-                } else {
-                    reverse[j] = (j/2);
-                }
+			/* if number over half, list repeats in pattern where every other
+			   number from index 0 decreases from N until mid 
+			   example: 5 x 4 x 3 */
+            if(K >= N/2) {
+                System.out.println((N-K-1) * 2);
+			/* else number is under half and every other number from index 1
+			   increases from 1 until mid (mid excluded)
+			   example: x 1 x 2 x*/
+            } else {
+                System.out.println(K * 2 + 1);
             }
-            System.out.println(reverse[K]);
         }
     }
 }
