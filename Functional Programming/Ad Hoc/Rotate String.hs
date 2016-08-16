7d5b4b2f5f895fe0e6abfe20d9bcca569e0907e4
Paul Bartlett <bartlett_paul@hotmail.com>
2016-08-15 21:03:29 -0400

    Rotating is hard

--- Functional Programming/Ad Hoc/Rotate String.hs
+++ Functional Programming/Ad Hoc/Rotate String.hs
@@ -1,10 +1,26 @@
+import Control.Applicative
 import Control.Monad
+import System.IO
 
-rotate :: [a] -> [a]
-rotate (x:xs) = xs:x
 
+main :: IO ()
 main = do
-  input <- getLine
-  inputs <- replicateM (read input) getLine
-  rotate "string"
-  print inputs
+    n_temp <- getLine
+    let n = read n_temp :: Int
+    forM_ [1..n] $ \a0  -> do
+        s <- getLine
+        rotate 1 [s]
+        
+rotate :: Int -> [a] -> [a]
+rotate _ [] = []
+rotate n xs = zipWith const (drop n (cycle xs)) xs
+
+getMultipleLines :: Int -> IO [String]
+
+getMultipleLines n
+    | n <= 0 = return []
+    | otherwise = do          
+        x <- getLine         
+        xs <- getMultipleLines (n-1)    
+        let ret = (x:xs)    
+        return ret 
