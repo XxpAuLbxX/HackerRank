4e4e24c37a1f1c0a6eb6ab721a1058bc8efb1ba1
paul-bartlett <pbartle7@uwo.ca>
2018-10-04 17:49:48 -0400

    Alternating characters challenge complete

---
+++ Interview Preparation Kit/String Manipulation/Alternating Characters.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the alternatingCharacters function below.
+    static int alternatingCharacters(String s) {
+        int len = s.length();
+        char prev = s.charAt(0);
+        int cnt = 0;
+        // Compare to previous char and "remove" if matched
+        for(int i = 1; i < len; i++) {
+            if(s.charAt(i) == prev) {
+                cnt++;
+            }
+            prev = s.charAt(i);
+        }
+        return cnt;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String s = scanner.nextLine();
+
+            int result = alternatingCharacters(s);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
