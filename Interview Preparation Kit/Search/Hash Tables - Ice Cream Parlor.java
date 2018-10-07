8a5fb8774b0cccd7949c75bc2d3c88857f981602
paul-bartlett <pbartle7@uwo.ca>
2018-10-06 20:59:46 -0400

    Hash Tables: Ice Cream Parlor challenge complete

---
+++ Interview Preparation Kit/Search/Hash Tables - Ice Cream Parlor.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the whatFlavors function below.
+    static void whatFlavors(int[] cost, int money) {
+        HashMap<Integer, Integer> hashCost = new HashMap<Integer, Integer>();
+        // Add costs to hashmap, also checking if it contains the other cost
+        for(int i = 0; i < cost.length; i++) {
+            int key = money - cost[i];
+            if(hashCost.containsKey(key)) {
+                System.out.println((hashCost.get(key)+1) + " " + (i+1));
+                break;
+            } else {
+                hashCost.put(cost[i], i);
+            }
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int money = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] cost = new int[n];
+
+            String[] costItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int costItem = Integer.parseInt(costItems[i]);
+                cost[i] = costItem;
+            }
+
+            whatFlavors(cost, money);
+        }
+
+        scanner.close();
+    }
+}
